9b8d1f39c6bc0e2d4b0292dd26d0584f8cec766a
gustuxd <gustavojsb@hotmail.com>
2019-08-15 21:49:46 -0300

    Projeto

---
+++ src/pkg/EstourarHeap.java
@@ -0,0 +1,51 @@
+package pkg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstourarHeap {
+	
+//	public static void main(String[] args) {
+//
+//		List<EstourarHeap> list = new ArrayList<EstourarHeap>();
+//		list.add(new EstourarHeap());
+//		
+//		while(true) {
+//			list.addAll(list);
+//			System.out.println(list.size());
+//		}
+//		
+//	}
+	
+//	public static void main(String[] args) {
+//		String a = "a";
+//		while(true)
+//			a = a + a;
+//	}
+	
+//	public static void main(String[] args) {
+//		int[][] vetor = new int[5000][];
+//		for(int i = 0; i < vetor.length; i++) {
+//			vetor[i] = new int[vetor.length * vetor.length];
+//		}
+//	}
+	
+	public static void main(String[] args) {
+		EstourarHeap eh = new EstourarHeap();
+		Node cabeca = eh.new Node();
+		Node n = cabeca;
+		while(n.prox == null) {
+			n.prox = eh.new Node();
+			n = n.prox;
+		}
+	}
+	
+	class Node {
+		
+		int[] vetor = new int[10000];
+		
+		Node prox;
+		
+	}
+	
+}
---
+++ src/pkg/EstourarStack.java
@@ -0,0 +1,13 @@
+package pkg;
+
+public class EstourarStack {
+
+	public static void main(String[] args) {
+		funcao();
+	}
+	
+	public static void funcao() {
+		funcao();
+	}
+	
+}
